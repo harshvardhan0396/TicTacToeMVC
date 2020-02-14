4ed975c30ba99bf83687cfdb6788b81b0d8fbf33
Harshvardhan Patidar <harshvardhan@mobiuso.com>
2020-02-14 12:44:04 +0530

    restart button working

--- TicTacToe/Controller/ViewController.swift
+++ TicTacToe/Controller/ViewController.swift
@@ -25,17 +25,41 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     var player = 1
-    
+    var count = 0
     @IBAction func restartButton(_ sender: UIButton) {
+        button00.setBackgroundImage(nil, for: UIControl.State.normal)
+        button01.setBackgroundImage(nil, for: UIControl.State.normal)
+        button02.setBackgroundImage(nil, for: UIControl.State.normal)
+        button10.setBackgroundImage(nil, for: UIControl.State.normal)
+        button11.setBackgroundImage(nil, for: UIControl.State.normal)
+        button12.setBackgroundImage(nil, for: UIControl.State.normal)
+        button20.setBackgroundImage(nil, for: UIControl.State.normal)
+        button21.setBackgroundImage(nil, for: UIControl.State.normal)
+        button22.setBackgroundImage(nil, for: UIControl.State.normal)
+        button00.isEnabled = true
+        button01.isEnabled = true
+        button02.isEnabled = true
+        button10.isEnabled = true
+        button11.isEnabled = true
+        button12.isEnabled = true
+        button20.isEnabled = true
+        button21.isEnabled = true
+        button22.isEnabled = true
     }
+    
     @IBAction func buttonPressed(_ sender: UIButton){
         if(player == 1 ){
             sender.setBackgroundImage(UIImage(named: "TTTO"), for: UIControl.State.normal)
             player = 2
+            sender.isEnabled = false
+            count += 1
         }
         else {
             sender.setBackgroundImage(UIImage(named: "TTTX"), for: UIControl.State.normal)
             player = 1
+            sender.isEnabled = false
+            count += 1
         }
+    print (count)
     }
 }
